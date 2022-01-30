55dec00d211523ede8e5a9ab0d3627f0148fad4b
merloss <54235902+Merloss@users.noreply.github.com>
2022-01-30 19:53:58 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   target: 'static',
   ssr: false,
+  generate: {
+    fallback: true,
+  },
   head: {
     title: 'Merloss',
     htmlAttrs: {
@@ -9,11 +12,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'Merloss',
-        content: 'Learn more about me',
-      },
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -43,9 +42,7 @@ export default {
 
   buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/svg', '@nuxtjs/color-mode'],
 
-  modules: ['@nuxtjs/axios'],
-
-  axios: {},
+  modules: [],
 
   build: {},
 
