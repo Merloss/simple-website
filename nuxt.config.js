a00852eb31a4f7af9739909fd996c8931063e2f2
merloss <54235902+Merloss@users.noreply.github.com>
2022-02-01 00:26:37 +0300

    metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '19 years old Turkish student, studying in Computer Programming and I try to learn new things every day in order to improve myself.',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
