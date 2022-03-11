8cf5425d4e995891f3ae27f2ba5b56bcb8898ead
merloss <54235902+Merloss@users.noreply.github.com>
2022-03-11 20:54:11 +0300

    Change background color, added dynamic gradient background

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,22 @@
   <div
     class="
       bg-gray-200
-      dark:bg-gray-800 dark:text-white
+      dark:bg-[#070510] dark:text-white
       overflow-hidden
-      transition
+      transition-all
       duration-75
       dark:selection:bg-white/10
       selection:bg-black/10
     "
+    :style="
+      this.$colorMode.value == 'dark'
+        ? `background-image: linear-gradient(to top left, #00000000,#00000000, ${this.$store.state.lanyard}40);`
+        : ''
+    "
   >
     <Nuxt class="container mx-auto justify-center sm:justify-start" />
     <navbar />
-    <div class="bg-gray-400 dark:bg-gray-700 fixed bottom-0 w-full py-1" />
+    <div class="bg-gray-400 dark:bg-[#130e2c] fixed bottom-0 w-full py-1" />
   </div>
 </template>
 <script>
