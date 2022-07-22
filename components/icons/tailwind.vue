c8ecdf3f0835edc79efb271d4e2e6f84db58cc73
merloss <54235902+Merloss@users.noreply.github.com>
2022-07-22 20:36:20 +0300

    converted each icon file to a component

---
+++ components/icons/tailwind.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="prefix__flex-shrink-0 prefix__h-7 prefix__w-7"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M6.333 9.933C7.088 6.911 8.978 5.4 12 5.4c4.533 0 5.1 3.4 7.367 3.967 1.511.377 2.833-.189 3.966-1.7-.755 3.022-2.644 4.533-5.666 4.533-4.534 0-5.1-3.4-7.367-3.967-1.511-.378-2.833.189-3.967 1.7zm-5.666 6.8C1.422 13.711 3.31 12.2 6.333 12.2c4.534 0 5.1 3.4 7.367 3.967 1.51.377 2.833-.19 3.967-1.7C16.91 17.489 15.022 19 12 19c-4.533 0-5.1-3.4-7.367-3.967-1.511-.378-2.833.189-3.966 1.7z"
+      fill="url(#prefix__prefix__paint0_linear)"
+    />
+    <defs>
+      <linearGradient
+        id="prefix__prefix__paint0_linear"
+        x1=".667"
+        y1="-6.689"
+        x2="23.333"
+        y2="31.089"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#2383AE" />
+        <stop offset="1" stop-color="#6DD7B9" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
