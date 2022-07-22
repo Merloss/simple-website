7ba4ca281d5f0a5e22cc6add34add01302d3f086
merloss <54235902+Merloss@users.noreply.github.com>
2022-07-22 21:31:59 +0300

    switched from tippy to tooltip

---
+++ plugins/tooltip.client.ts
@@ -0,0 +1,7 @@
+import FloatingVue from 'floating-vue'
+
+import 'floating-vue/dist/style.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(FloatingVue, { directory: 'tooltip' })
+})
